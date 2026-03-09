3ab161e960a666461158bd78de50d7a35620f754
vedv68 <vishnoivedvardhan@gmail.com>
2026-03-09 09:43:49 +0530

    UC2: Implement basic room types with static availability using abstraction and inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,65 @@
-public class Main {
+abstract class Main {
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 300);
+    }
+}
+
+public class UseCase2RoomInitialization {
 
     public static void main(String[] args) {
 
-        // Application name and version
-        String appName = "Hotel Booking System";
-        String version = "v1.0";
+        // Static availability variables
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
 
-        // Welcome message
-        System.out.println("=================================");
-        System.out.println("Welcome to the " + appName);
-        System.out.println("Version: " + version);
-        System.out.println("=================================");
-        System.out.println("Application started successfully.");
+        // Polymorphism
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // Program ends
+        System.out.println("===== Book My Stay App =====");
+        System.out.println("Available Room Types\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailable);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailable);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailable);
     }
 }
